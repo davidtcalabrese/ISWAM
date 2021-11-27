995cd245730ef9b72696b80a24aa3f31406959b7
David Calabrese <david.thomas.calabrese@gmail.com>
2021-11-26 22:07:35 -0600

    refactors settings file into one function

--- js/sessionSettings.js
+++ js/sessionSettings.js
@@ -1,20 +1,22 @@
 /**
- * Gets the user input for severity on settings page and sets in session storage
+ * Gets the user input for severity and color on settings page
+ * and sets or resets these variables in session storage.
  */
- const setSeverity = () => {
+const updateUserSettings = () => {
   const severity = document.querySelector('#severity').value;
-
-  sessionStorage.setItem('severity', severity);
-};
-
-/**
- * Gets the user input for severity on settings page and sets in session storage
- */
- const setLightColor = () => {
   const color = document.querySelector('#light-color').value;
 
+  sessionStorage.setItem('severity', severity);
   sessionStorage.setItem('color', color);
-};
 
-document.querySelector('#save').addEventListener('click', setSeverity);
-document.querySelector('#light-color').addEventListener('click', setLightColor);
+  const confirmationMsg = `
+    <div class="col">
+      <p class="text-center text-success">Your settings have been saved.</p>
+    </div>
+  `;
+
+  document.querySelector("#confirmation").innerHTML = confirmationMsg;
+}
+
+document.querySelector('#save').addEventListener('click', updateUserSettings);
+
