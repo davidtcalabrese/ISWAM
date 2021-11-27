ea1e42b488e8d97f3250acde7bc55d2f9dbc8d52
David Calabrese <david.thomas.calabrese@gmail.com>
2021-11-27 15:49:37 -0600

    adds UI_settings file

---
+++ js/UI_settings.js
@@ -0,0 +1,21 @@
+  /**
+   * Gets the user input for severity and color on settings page
+   * and sets or resets these variables in session storage.
+   */
+   const updateUserSettings = () => {
+    const severity = document.querySelector('#severity').value;
+    const color = document.querySelector('#light-color').value;
+
+    sessionStorage.setItem('severity', severity);
+    sessionStorage.setItem('color', color);
+
+    const confirmationMsg = `
+      <div class="col">
+        <p class="text-center text-success">Your settings have been saved.</p>
+      </div>
+    `;
+
+    document.querySelector('#confirmation').innerHTML = confirmationMsg;
+  };
+
+document.querySelector('#save').addEventListener('click', updateUserSettings);
