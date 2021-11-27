92ba7eda073dbf101a8cdacfe0cfefefbcce4089
David Calabrese <david.thomas.calabrese@gmail.com>
2021-11-26 21:54:50 -0600

    adds fxn for setting light session var

--- js/sessionSettings.js
+++ js/sessionSettings.js
@@ -7,4 +7,14 @@
   sessionStorage.setItem('severity', severity);
 };
 
+/**
+ * Gets the user input for severity on settings page and sets in session storage
+ */
+ const setLightColor = () => {
+  const color = document.querySelector('#light-color').value;
+
+  sessionStorage.setItem('color', color);
+};
+
 document.querySelector('#save').addEventListener('click', setSeverity);
+document.querySelector('#light-color').addEventListener('click', setLightColor);
