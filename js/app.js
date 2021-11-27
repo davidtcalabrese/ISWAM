4ad9a851a5b5b9837ad7ab0c4f269643a1c8e51a
David Calabrese <david.thomas.calabrese@gmail.com>
2021-11-27 15:49:22 -0600

    moves init to util.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
-'use strict';
+// 'use strict';
 
-/**
- * Initializes app by accessing form fields and passing them to checkWeatherAndAlerts.
- * 
- * @author David Calabrese <david.thomas.calabrese@gmail.com>
- */
-const init = async function () {
-  const UI = new UI();
-  const zip = UI.getZip();
-  const severity = getSeverity();
-  checkWeatherAndAlerts(zip, severity);
-};
+// /**
+//  * Initializes app by accessing form fields and passing them to checkWeatherAndAlerts.
+//  * 
+//  * @author David Calabrese <david.thomas.calabrese@gmail.com>
+//  */
+// const init = async function () {
+//   const UI = new UI();
+//   const zip = UI.getZip();
+//   const severity = getSeverity();
+//   checkWeatherAndAlerts(zip, severity);
+// };
+
