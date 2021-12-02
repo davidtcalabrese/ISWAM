2037d047fcbfbd6bbdc34bbf24a0a68aded09238
David Calabrese <david.thomas.calabrese@gmail.com>
2021-12-02 11:01:42 -0600

    removes logs, cleans up blank lines

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,21 +3,13 @@ const router = express.Router();
 const { processWeather } = require('../controllers/weatherFunctions.js');
 const { processAlert } = require('../controllers/alertFunctions.js');
 
-
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-
-});
+router.get('/', function(req, res, next) {});
 
 router.post('/', async (req, res, next) => {
   const zip = req.body.zip;
-
-  const weatherData = await processWeather(zip);
-  
+  const weatherData = await processWeather(zip); 
   const alertData = await processAlert(zip, 1);
-
-  console.log(alertData);
 
   if (alertData === -1) {
     var weatherAndAlertData = weatherData;
@@ -25,8 +17,6 @@ router.post('/', async (req, res, next) => {
     weatherAndAlertData = [...alertData, ...weatherData];
   }
   res.json(weatherAndAlertData);
-
 });
-
 
 module.exports = router;
