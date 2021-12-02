67a5d8936ac3cb4f9824944481f1f03de1508d68
David Calabrese <david.thomas.calabrese@gmail.com>
2021-12-02 08:16:41 -0600

    adds MIME types to accept header

--- client/js/util.js
+++ client/js/util.js
@@ -11,8 +11,8 @@ async function postData(url = '', data = {}) {
   const response = await fetch(url, {
     method: 'POST',  
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify(data)  
   });
