c50690382cd56a2cb5c6d221220fa5ea37a4ab88
David Calabrese <david.thomas.calabrese@gmail.com>
2021-12-02 20:06:55 -0600

    adds color as param in call to processAlert

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,8 +9,10 @@ router.get('/', function(req, res, next) {});
 router.post('/', async (req, res, next) => {
   const zip = req.body.zip;
   const severity = req.body.severity;
+  const color = req.body.color;
+
   const weatherData = await processWeather(zip); 
-  const alertData = await processAlert(zip, severity);
+  const alertData = await processAlert(zip, severity, color);
 
   if (alertData === -1) {
     var weatherAndAlertData = weatherData;
