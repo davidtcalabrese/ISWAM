a1529e669a89124d8f9b0785d1c08eb1b6379715
David Calabrese <david.thomas.calabrese@gmail.com>
2021-12-06 20:51:23 -0600

    lowers rgb values for alert default
    sending out alerts at 255, 255, 255 for white seemed to overload the leds and not all of them would light. I am adjusting white color to 211x3, which is still a nice white color

--- client/js/util.js
+++ client/js/util.js
@@ -279,7 +279,7 @@ async function postData(url = '', data = {}) {
  * @returns {string} String representing a color.
  */
  const getColor = () => {
-  const color = sessionStorage.getItem('color') || "255,255,255";
+  const color = sessionStorage.getItem('color') || "211,211,211";
 
   return color;
 };
