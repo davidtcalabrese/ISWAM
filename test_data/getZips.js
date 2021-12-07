3a1524872b49af21863b456f2cc93b540e375954
David Calabrese <david.thomas.calabrese@gmail.com>
2021-12-07 17:45:21 -0600

    adjusts alert zipcode generator script

--- test_data/getZips.js
+++ test_data/getZips.js
@@ -2,18 +2,19 @@ import fetch from 'node-fetch';
 import * as fs from 'fs';
 import readline from 'readline';
 import { config } from './test_config.js';
+import axios from 'axios';
 
 /**
- * This file contains functions for getting every zip code for 
- * which there is an active alert. 
- * 
+ * This file contains functions for getting every zip code for
+ * which there is an active alert.
+ *
  * Run `node getZips` to get a list of zips written to text file.
  */
 
 /**
  * Given a Universal Geographic Code (UGC), returns geographic coordinates.
- * 
- * @param {string} UGC 
+ *
+ * @param {string} UGC
  * @returns {string} geographic coordinates in format lat,long
  */
 const getCoordsFromUGC = async function (UGC) {
@@ -45,7 +46,7 @@ const getCoordsFromUGC = async function (UGC) {
 
 /**
  * Given a pair of geographic coordinates, returns a 5 digit zip code.
- * 
+ *
  * @param {string} coords - geographic coordinates in format lat,long
  * @returns {string} a five digit zip vode
  */
@@ -79,8 +80,8 @@ const getZipFromCoords = async function (coords) {
 /**
  * Takes in a UGC, calls API to get coordinates, passes those coordinates
  * to another API to get zip code. Prints zip codes to file named zips.txt.
- * 
- * @param {string} UGC 
+ *
+ * @param {string} UGC
  */
 const printZips = async function (UGC) {
   const coords = await getCoordsFromUGC(UGC);
@@ -89,9 +90,9 @@ const printZips = async function (UGC) {
   if (typeof zip === 'undefined') {
     return;
   }
-  if (zip.length !== 5) {
+  if (zip.length !== 5 || !zipHasAlert()) {
     return;
-  } 
+  }
 
   const line = `${zip}\n`;
 
@@ -105,7 +106,29 @@ const printZips = async function (UGC) {
 };
 
 /**
- * Prints header row for CSV file single time. Then loops through 
+ * Accesses the Weatherbit API, retrieves alert for area or -1 if no alert.
+ *
+ * @param {string} zip - A zip code identifying a region.
+ * @returns {Object} - Alert object from api or -1 if no alert.
+ */
+const zipHasAlert = async zip => {
+  const API_KEY = config.API_KEY;
+  const resp = await axios.get(`https://api.weatherbit.io/v2.0/alerts?country=US&postal_code=${zip}&key=${API_KEY}`);
+  const data = await resp.data;
+  if (typeof data.alerts === 'undefined') {
+    return false;
+  }
+  const alert = data.alerts[0];
+
+  // return -1 if no alert for zip
+  if (typeof alert === 'undefined') {
+    return false;
+  } // otherwise return the alert
+  return true;
+};
+
+/**
+ * Prints header row for CSV file single time. Then loops through
  * UGC array, passing each to printUGCCoordsAndZipAsCSV
  */
 const readUGCs = async function () {
@@ -114,21 +137,22 @@ const readUGCs = async function () {
 
   const rl = readline.createInterface({
     input: fileStream,
-    crlfDelay: Infinity
+    crlfDelay: Infinity,
   });
 
   for await (const UGC of rl) {
     printZips(UGC);
   }
+  filterDuplicates()
 };
 
 /**
- * Accesses every active NWS alert (on land), parses each into a simplified json format, 
- * and writes to alert_data.txt for test data. 
+ * Accesses every active NWS alert (on land), parses each into a simplified json format,
+ * and writes to alert_data.txt for test data.
  */
- const getAllAlerts = async function () {
+const getAllAlerts = async function () {
   const URL = `https://api.weather.gov/alerts/active?status=actual&message_type=alert&region_type=land`;
-  
+
   const requestOptions = {
     method: 'GET',
     redirect: 'follow',
@@ -140,18 +164,40 @@ const readUGCs = async function () {
 
   // grab props: event, severity, description, onset, ends
   for (let alert of alertArray) {
-
-    const code = alert.properties.geocode.UGC[0] + " \n";
+    const code = alert.properties.geocode.UGC[0] + ' \n';
 
     fs.appendFile('alert_UGCs.txt', code, err => {
-      if (err) { console.log(err); } 
-      else { console.log('UGC added'); }
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('UGC added');
+      }
     });
   }
 
   readUGCs();
 };
 
+const filterDuplicates = async function () {
+  try {
+    let data = fs.readFileSync('zips.txt', 'utf8');
+
+    // split data by tabs, newlines and spaces
+    data = data.toString().split(/[\n \t ' ']/);
+
+    // this will remove duplicates from the array
+    const result = data.filter((item, pos) => data.indexOf(item) === pos);
+
+    const file = fs.createWriteStream('zipSet.txt');
+    file.on('error', function(err) { console.log(err); });
+    result.forEach(function(zip) { file.write(zip + '\n'); });
+    file.end();
+  } catch (e) {
+    console.log('Error:', e.stack);
+  }
+};
+
+
+
+
 getAllAlerts();
-
-
