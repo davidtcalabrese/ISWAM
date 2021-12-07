7ce730c73a7674e06a8d1c25600fff4614e8c21f
David Calabrese <david.thomas.calabrese@gmail.com>
2021-12-06 21:55:06 -0600

    breaks off some route logic to util file

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,26 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { processWeather } = require('../controllers/weatherFunctions.js');
-const { processAlert } = require('../controllers/alertFunctions.js');
+const { pollFunc, callApis } = require('./routeUtil.js');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {});
-
-router.post('/', async (req, res, next) => {
-  const zip = req.body.zip;
-  const severity = req.body.severity;
-  const color = req.body.color;
-
-  const weatherData = await processWeather(zip); 
-  const alertData = await processAlert(zip, severity, color);
-
-  if (alertData === -1) {
-    var response = weatherData;
-    response.alertPresent = false;
-  } else {
-    response = {...weatherData, ...alertData};
-    response.alertPresent = true;
-  }
+router.post('/', async (req, res) => {
+  const response = await pollFunc(callApis, 300, 15, req);
+  
   res.json(response);
 });
 
