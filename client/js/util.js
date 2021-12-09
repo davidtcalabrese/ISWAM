402a9df7ac5068eab6848b963d6aafe74e3bb319
David Calabrese <david.thomas.calabrese@gmail.com>
2021-12-09 15:18:39 -0600

    adjusts white rgb values

--- client/js/util.js
+++ client/js/util.js
@@ -280,7 +280,7 @@ async function postData(url = '', data = {}) {
  * @returns {string} String representing a color.
  */
  const getColor = () => {
-  const color = sessionStorage.getItem('color') || "211,211,211";
+  const color = sessionStorage.getItem('color') || "140,140,140";
 
   return color;
 };
